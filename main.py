09db7b61b609467cd7326b23f468974c345711ca
Georgi Dimov <dimov.georgi1998@gmail.com>
2024-10-18 07:50:45 -0700

    Video Search using keyword. Returning 5 video titles and urls

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+from logging import fatal
+
 import requests
 from pytube import YouTube
 
-
-API_KEY = 'AIzaSyAPb3-Oq4UCVP35lvBSK6Htso2EwHxbpo8'
+API_KEY = ''
 BASE_URL = 'https://www.googleapis.com/youtube/v3/search'
+
+
+
+def video_search(keyword):
+    params = {
+        "part": "id,snippet",
+        "key": API_KEY,
+        "q": keyword,
+        "maxResults": 5,
+        "type": "video",
+    }
+
+    r = requests.get(BASE_URL, params=params)
+    result = []
+    for number, data in enumerate(r.json()["items"]):
+        video_title = data["snippet"]["title"]
+        video_id = data["id"]["videoId"]
+        video_url = f"https://www.youtube.com/watch?v={video_id}"
+        result.append(f'{number + 1}: {video_title} --> {video_url}')
+    return "\n".join(result)
+
+keyword = input("Въведете дума за търсене: ")
+print(video_search(keyword))
+
+
+
+
